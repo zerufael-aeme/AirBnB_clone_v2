ae385d332f20ecefbdf7835e1eed200f35a18589
Zerufael Aemero <zerufaelaemero@gmail.com>
2024-05-10 07:35:51 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,31 +5,26 @@
 import os
 from fabric.api import run, put, env
 
-env.hosts = ['44.192.38.3', '3.239.82.120']
+env.hosts = ['52.91.182.253', '52.201.229.108']
 env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """Create a tar gzipped archive of the directory web_static."""
-    if os.path.exists(archive_path) is False:
+
+    # Checks if the archive path exists
+    if not os.path.exists(archive_path):
+        print("Archive path not found")
         return False
-    else:
-        try:
-            put(archive_path, "/tmp/")
-            """ putting the file to .tgz """
-            file_name = archive_path.split("/")[1]
-            """ splitting .tgz """
-            file_name2 = file_name.split(".")[0]
-            """ spliting archivo """
-            final_name = "/data/web_static/releases/" + file_name2 + "/"
-            run("mkdir -p " + final_name)
-            run("tar -xzf /tmp/" + file_name + " -C " + final_name)
-            run("rm /tmp/" + file_name)
-            run("mv " + final_name + "web_static/* " + final_name)
-            run("rm -rf " + final_name + "web_static")
-            run("rm -rf /data/web_static/current")
-            run("ln -s " + final_name + " /data/web_static/current")
-            print("New version deployed!")
-            return True
-        except Exception:
-            return False
+    
+    archive_name = os.path.basename(archive_path)
+    
+    try:
+        with prefix(put(archive_path, "/tmp/")):
+            run("bsdtar -xf /tmp/{} -C /data/web_static/releases/".format(archive_name))
+            run("rm /tmp/{}".format(archive_name))
+            run("rm /data/web_static/current")
+            run("ln -sf /data/web_static/releases/{} /data/web_static/current".format(archive_name))
+    except Exception as e:
+        print("Operation Failed: {}".format(e))
+        return False
+    return True
